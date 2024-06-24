b9224670ddf2fbb898355b5b872ca7a71bc4a91b
johnelipse <johnelipse472@gmail.comgit config --global user.name johnelipseOmit --globalgit commit -meventgit config --global user.email johnelipse472@gmail.com>
2024-06-24 18:57:32 +0300

    ui-update

--- app/page.tsx
+++ app/page.tsx
@@ -22,20 +22,20 @@ export default function page() {
         <h3 className="text-white text-[1.2rem] ">Response</h3>
         <div className="mt-4 text-white border-[1px] px-2 py-3 w-full">
         
-     ["_id": "6679761401262827c937209b",<br/>
-      "title": "Dom Manipulation",<br/>
-      "description": "This is the descripton for Assignment 1",<br/>
-      "image": "https://coding-school-typescript.vercel.app/_next/image?url=https%3A%2F%2Fpublic-files.gumroad.com%2Fzb8tbbmhjq2rwe7n326kkdxw2hwx&w=1920&q=75",<br/>
-      "link": "https://desishub-docs.vercel.app/programming-tutorials/nextj",<br/>
-      "__v": 0]<br/>
+     [_id: 6679761401262827c937209b,<br/>
+      title: Dom Manipulation,<br/>
+      description: This is the descripton for Assignment 1,<br/>
+      image: https://coding-school-typescript.vercel.app/_next/image?url=https%3A%2F%2Fpublic-files.gumroad.com%2Fzb8tbbmhjq2rwe7n326kkdxw2hwx&w=1920&q=75,<br/>
+      link: https://desishub-docs.vercel.app/programming-tutorials/nextj,<br/>
+      __v: 0]<br/>
     
     
-      ["_id": "6679774501262827c937209e",<br/>
-      "title": "AirMax Product Detail Page",<br/>
-      "description": "In this challenge, you'll build a beautiful product page.",<br/>
-      "image": "https://www.desishub.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FAirMaxBlackSnikers.dc17e7a1.jpg&w=828&q=75",<br/>
-      "link": "https://www.desishub.com/desishub-coding-school/projects-and-challenges/landing-page-design",<br/>
-      "__v": 0]
+      [_id: 6679774501262827c937209e,<br/>
+      title: AirMax Product Detail Page,<br/>
+      description: In this challenge, you'll build a beautiful product page.,<br/>
+      image: https://www.desishub.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FAirMaxBlackSnikers.dc17e7a1.jpg&w=828&q=75,<br/>
+      link: https://www.desishub.com/desishub-coding-school/projects-and-challenges/landing-page-design,<br/>
+      __v: 0]
     
         </div>
       </div>
@@ -54,12 +54,12 @@ export default function page() {
         <h3 className="text-white text-[1.2rem] ">Response</h3>
         <div className="mt-4 text-white border-[1px] px-2 py-3">
         
-     ["_id": "6679761401262827c937209b",<br/>
-      "title": "Dom Manipulation",<br/>
-      "description": "This is the descripton for Assignment 1",<br/>
-      "image": "https://coding-school-typescript.vercel.app/_next/image?url=https%3A%2F%2Fpublic-files.gumroad.com%2Fzb8tbbmhjq2rwe7n326kkdxw2hwx&w=1920&q=75",<br/>
-      "link": "https://desishub-docs.vercel.app/programming-tutorials/nextj",<br/>
-      "__v": 0]<br/>
+     [_id: 6679761401262827c937209b,<br/>
+      title: Dom Manipulation,<br/>
+      description: This is the descripton for Assignment 1,<br/>
+      image: https://coding-school-typescript.vercel.app/_next/image?url=https%3A%2F%2Fpublic-files.gumroad.com%2Fzb8tbbmhjq2rwe7n326kkdxw2hwx&w=1920&q=75,<br/>
+      link: https://desishub-docs.vercel.app/programming-tutorials/nextj,<br/>
+      __v: 0]<br/>
        </div>
       </div>
         </div>
@@ -77,12 +77,12 @@ export default function page() {
         <h3 className="text-white text-[1.2rem] ">Body</h3>
         <div className="mt-4 text-white border-[1px] px-2 py-3">
         
-     ["_id": "6679761401262827c937209b",<br/>
-      "title": "Dom Manipulation",<br/>
-      "description": "This is the descripton for Assignment 1",<br/>
-      "image": "https://coding-school-typescript.vercel.app/_next/image?url=https%3A%2F%2Fpublic-files.gumroad.com%2Fzb8tbbmhjq2rwe7n326kkdxw2hwx&w=1920&q=75",<br/>
-      "link": "https://desishub-docs.vercel.app/programming-tutorials/nextj",<br/>
-      "__v": 0]<br/>
+     [_id: 6679761401262827c937209b,<br/>
+      title": "Dom Manipulation,<br/>
+      description: This is the descripton for Assignment 1,<br/>
+      image: https://coding-school-typescript.vercel.app/_next/image?url=https%3A%2F%2Fpublic-files.gumroad.com%2Fzb8tbbmhjq2rwe7n326kkdxw2hwx&w=1920&q=75,<br/>
+      link: "https://desishub-docs.vercel.app/programming-tutorials/nextj,<br/>
+      __v: 0]<br/>
        </div>
       </div>
         </div>
